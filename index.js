be3649f03c4b6cf424f25dee64c296fc7e204d75
kuvanch <kolyabek13@gmail.com>
2021-02-26 21:45:01 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express =  require('express')
+const app = express()
+const exphbs = require('express-handlebars')
+
+const hbs = exphbs.create({
+    defaultLayout:'main',
+    extname:'hbs'
+})
+
+
+app.engine('hbs', hbs.engine)
+
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.static('public'))
+
+app.get('/', (req,res) => {
+    res.render('index')
+})
+app.get('/about', (req,res) => {
+    res.render('about')
+})
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+    console.log('Server is running on',PORT);
+})
